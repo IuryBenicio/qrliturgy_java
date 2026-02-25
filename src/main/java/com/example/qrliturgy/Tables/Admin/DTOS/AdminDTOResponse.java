7f07f90b54cc38fcc3f49d28f58a025bbc6e58ad
Iury Benicio <iury620@gmail.com>
2026-02-25 11:22:00 -0300

    Fix: consertando verificação de registro e login

--- src/main/java/com/example/qrliturgy/Tables/Admin/DTOS/AdminDTOResponse.java
+++ src/main/java/com/example/qrliturgy/Tables/Admin/DTOS/AdminDTOResponse.java
@@ -1,9 +1,7 @@
 package com.example.qrliturgy.Tables.Admin.DTOS;
 
-import jakarta.validation.constraints.NotBlank;
-
-public record AdminDTOResponse(
-        @NotBlank String nome,
-        @NotBlank String email
+public record AdminDTOLoginResponse(
+        AdminDTOResponse user,
+        String message
 ) {
 }
